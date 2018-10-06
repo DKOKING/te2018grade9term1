82c657e75a3a66ec386c3371830c842ee338b1b5
Kristian Ofori <kristian.ofori@csedge.org>
2018-10-06 15:45:45 -0400

    Challenge 1: Complete
    It was completed

--- data.js
+++ data.js
@@ -1,5 +1,24 @@
 // Example
+const READLINE = require("readline-sync")
 let message = "Hello World!";
+let age = 14;
+let month = 10;
+let day = 2;
+let yo = "Yo, how ya doin'?";
+let bye = "See ya later!";
+let doILikeAnime = true;
+let isWaterWet = false;
+let k;
+let s = "sonic";
+let userInput = READLINE.question("Oi Oi bois!")
+console.log(age);
+console.log(month);
+console.log(day);
 console.log(message);
+console.log(doILikeAnime);
+console.log(isWaterWet);
+console.log(k);
+console.log(typeof s);
+console.log('You typed:${userInput}');
 
 // Type your code below!
